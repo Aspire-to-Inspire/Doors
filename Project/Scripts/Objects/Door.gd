71f57e6f4ff7adb84bbe0048e221e29fb3437800
Crystalwarrior <Varsash@gmail.com>
2019-11-23 18:01:05 +0300

    Timer, Door, Health, whatever

--- Project/Scripts/Objects/Door.gd
+++ Project/Scripts/Objects/Door.gd
@@ -1,9 +1,13 @@
 extends StaticBody2D
 
-var health = 100
+export var health = 50
 
 func collided(player):
 	health -= 1
-	print(health)
 	if health <= 0:
 		queue_free()
+		var progress = get_tree().get_root().get_node("Main/UI/Timer")
+		var timer = progress.get_node("Timer")
+		timer.wait_time = timer.time_left + 5
+		progress.max_value = timer.wait_time
+		timer.start()
---
+++ Project/Scripts/Timer.gd
@@ -0,0 +1,11 @@
+extends ProgressBar
+
+func _ready():
+	value = $Timer.wait_time
+	max_value = value
+
+func _process(delta):
+	value = $Timer.time_left
+
+func _on_Timer_timeout():
+	print("you are dead, dead, dead")
