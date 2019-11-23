997115d16a49d197634642ab45ab516bb654fc91
Alex Noir <Varsash@gmail.com>
2019-11-23 11:07:21 +0300

    Исправил баг с фейдом уровня Изменил позицию джойстика на экране и сделал тач-зону на весь экран ТОДО: исправить один фрейм предыдущей сцены при переходе уровней

--- Project/Scripts/TransitionZone.gd
+++ Project/Scripts/TransitionZone.gd
@@ -6,4 +6,4 @@ export var target_spawn_point = 0
 func do_what_this_object_does():
 	GameData.zone_load_spawn_point = target_spawn_point
 	GameData.zone_load_facing_direction = InputSystem.input_direction
-	UI.fade_transition_scene(target_scene)
+	GUI.fade_transition_scene(target_scene)
