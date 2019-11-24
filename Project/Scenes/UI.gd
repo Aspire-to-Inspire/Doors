efa83d1eec799de0daa7b5a605b3e4d44d217810
Осипян Арам <osipyan.aram2017@ya.ru>
2019-11-24 15:45:11 +0300

    ogg

--- Project/Scenes/UI.gd
+++ Project/Scenes/UI.gd
@@ -9,11 +9,13 @@ func _ready():
 func _on_Pause_pressed():	
 	get_tree().paused = true
 	get_node("Resume").visible = true
-	resume_music.play()
+	if resume_music:
+		resume_music.play()
 	
 
 
 func _on_Resume_pressed():
 	get_tree().paused = false
 	get_node("Resume").visible = false
-	resume_music.stop()
+	if resume_music:
+		resume_music.stop()
