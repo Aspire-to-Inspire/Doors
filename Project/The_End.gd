7b3d32a0a6c97bca6b97eba9c0d7909e4cbe85e9
Осипян Арам <osipyan.aram2017@ya.ru>
2019-11-24 13:26:43 +0300

    исправил ошибки

--- Project/The_End.gd
+++ Project/The_End.gd
@@ -4,7 +4,8 @@ func _ready():
 	get_tree().get_root().get_node("Main/Player").connect("UR_DED",self,"print_when_ur_ded")
 
 func print_when_ur_ded():
-	print("UR_DED")
+	#print("UR_DED")
+	get_tree().paused = true
 	self.visible = true
 	
 func _on_Button_pressed():
