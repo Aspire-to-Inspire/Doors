2e4d218070edf981058295ddff903c0b0d5eedbd
Edcurrock <edgtg0304@gmail.com>
2020-02-15 17:38:15 +0300

    Add WindLevel Add Wind Mechanics - WindArea

--- Doors/Project/Scenes/Levels/WindLevel.gd
+++
@@ -1,6 +0,0 @@
-extends "res://Scripts/Levels/Base.gd"
-onready var Player = get_tree().get_root().get_node("Main/Player")
-export var speed_of_wind = 70
-
-func _physics_process(delta):
-	Player.move_and_slide(Vector2(1,0)*speed_of_wind)
---
+++ Doors/Project/Scenes/Objects/WindArea.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+const actor = preload("res://Scripts/Actors/Actor.gd")
+	
+signal onWind
+var is_active = false
+var this_body
+
+func _physics_process(delta):
+	if is_active:
+		this_body.move_and_slide(self.transform.y*70)
+	
+	
+func _on_WindArea_body_entered(body):
+	if(body is actor):
+		this_body = body
+		is_active = true
+
+
+
+func _on_WindArea_body_exited(body):
+	if(body is actor):
+		is_active = false
