da3440657456b85780e260ecf9d270f9961ca6b9
Осипян Арам <osipyan.aram2017@ya.ru>
2020-03-04 17:59:10 +0300

    cделал retry

--- Doors/Project/Scripts/UI.gd
+++ Doors/Project/Scripts/UI.gd
@@ -9,9 +9,16 @@ func _on_Pause_pressed():
 	get_tree().paused = true
 	get_node("Resume").visible = true
 	get_node("Button").visible = true
+	get_node("Retry").visible = true
 
 
 func _on_Resume_pressed():
 	get_tree().paused = false
 	get_node("Resume").visible = false
 	get_node("Button").visible = false
+	get_node("Retry").visible = false
+
+
+func _on_Retry_pressed():
+	get_tree().paused = false
+	get_tree().change_scene("res://Scenes/Menu.tscn")
