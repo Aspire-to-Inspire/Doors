f3f10a7395919753ecb10df9f22272c916d2209f
Осипян Арам <osipyan.aram2017@ya.ru>
2020-04-22 21:22:45 +0300

    черновой leaderboard

--- Doors/Project/Menu.gd
+++ Doors/Project/Menu.gd
@@ -4,7 +4,15 @@ class_name Menu
 onready var menu_music = $music
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	#SilentWolf.Scores.persist_score("Aram", 12)
+	#SilentWolf.Scores.persist_score("Ivan", 5)
+	#SilentWolf.Scores.persist_score("Edgar", 15)
+	#SilentWolf.Scores.persist_score("Sacha", 9)
+	#SilentWolf.Scores.persist_score("Katya", 20)
+	yield(SilentWolf.Scores.get_high_scores(0), "sw_scores_received")
+	print("Scores: " + str(SilentWolf.Scores.scores))   
+	  
+	#pass
 	#get_tree().get_root().get_node("")
 #	menu_music.play()
 
