af827ff567b96c8b3e32db4bbdb2f065dacff7bd
Осипян Арам <osipyan.aram2017@ya.ru>
2020-04-23 20:31:55 +0300

    сделал leaderboard с сохранением

--- Doors/Project/Menu.gd
+++ Doors/Project/Menu.gd
@@ -9,8 +9,8 @@ func _ready():
 	#SilentWolf.Scores.persist_score("Edgar", 15)
 	#SilentWolf.Scores.persist_score("Sacha", 9)
 	#SilentWolf.Scores.persist_score("Katya", 20)
-	yield(SilentWolf.Scores.get_high_scores(0), "sw_scores_received")
-	print("Scores: " + str(SilentWolf.Scores.scores))   
+	pass
+	   
 	  
 	#pass
 	#get_tree().get_root().get_node("")
@@ -23,3 +23,7 @@ func _on_Button_pressed():
 	get_tree().change_scene("res://Scenes/Main.tscn")
 	print("button pressed")
 
+
+
+func _on_leaderboard_pressed():
+	get_tree().change_scene("res://LeaderBoard.tscn")
